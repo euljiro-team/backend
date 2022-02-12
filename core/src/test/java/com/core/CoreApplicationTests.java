ef3fb140f4150eee6af268c51512c9b27c20a736
minssan9 <minssan9@gmail.com>
2022-02-12 12:20:05 +0900

    (feat) test sample

---
+++ core/src/test/java/com/core/CoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.core;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CoreApplicationTests {
+    public void contextLoads(){
+
+    }
+}
---
+++ core/src/test/java/com/core/euljiro/service/MetaTableServiceTest.java
@@ -0,0 +1,33 @@
+package com.core.euljiro.service;
+
+import com.core.euljiro.domain.MetaTable;
+import com.core.euljiro.dto.MetaTableDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("local")
+class MetaTableServiceTest {
+
+    @Autowired
+    MetaTableService metaTableService;
+
+
+    @Test
+    void getById() {
+        MetaTableDTO metaTableDTO =  metaTableService.getById("AREA");
+//        assertEquals(metaTableDTO, this.metaTableDTO);
+        assertNotNull(metaTableDTO);
+
+    }
+}
